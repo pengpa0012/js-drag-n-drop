0334fcbea7ec34bb8a02bff2033ce7cfbeabca06
Pengpa0012 <peng.ilikha@gmail.com>
2024-01-30 07:10:37 +0800

    added modal

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const items = document.querySelectorAll(".item")
+const form = document.querySelector("#form")
 const sections = document.querySelectorAll(".section")
+const overlay = document.querySelector(".overlay")
+const addTask = document.querySelector(".add-task")
+
+document.addEventListener('click', e => {
+  if (!form.contains(e.target) && !addTask.contains(e.target)) {
+    overlay.classList.add("hide")
+    overlay.classList.remove("show")
+  }
+})
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+
+  console.log(e)
+
+  overlay.classList.add("hide")
+  overlay.classList.remove("show")
+})
+
+addTask.addEventListener("click", () => {
+  overlay.classList.add("show")
+  overlay.classList.remove("hide")
+})
 
 items.forEach(el => {
-  el.addEventListener("dragstart", (e) => {
+  el.addEventListener("dragstart", e => {
+    e.target.classList.add('dragging')
     e.target.classList.add("opacity-50")
     console.log(e)
   })
 
-
-  // append child on container
-  el.addEventListener("dragend", (e) => {
-    const containerItem = e.target.closest(".section")
-    el.classList.add("hidden")
-    const newEl = document.createElement("div")
-    newEl.className = "item border p-2 rounded-md bg-[#e4e4e4] w-full cursor-grab"
-    newEl.textContent = "ITEM"
-    containerItem.appendChild(newEl)
+  el.addEventListener("dragend", e => {
+    e.target.classList.remove('dragging')
+    e.target.classList.remove("opacity-50")
   })
 })
 
+sections.forEach(el => {
+  el.addEventListener('dragover', e => {
+    const draggable = document.querySelector('.dragging')
+    el.appendChild(draggable)    
+  })  
+})
