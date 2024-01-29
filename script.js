36d20df5f8dfa8a2afe08defd131d6778d38d570
Pengpa0012 <peng.ilikha@gmail.com>
2024-01-29 22:37:25 +0800

    added initial drag

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Test")
+const items = document.querySelectorAll(".item")
+const sections = document.querySelectorAll(".section")
+
+items.forEach(el => {
+  el.addEventListener("dragstart", (e) => {
+    e.target.classList.add("opacity-50")
+    console.log(e)
+  })
+
+
+  // append child on container
+  el.addEventListener("dragend", (e) => {
+    const containerItem = e.target.closest(".section")
+    el.classList.add("hidden")
+    const newEl = document.createElement("div")
+    newEl.className = "item border p-2 rounded-md bg-[#e4e4e4] w-full cursor-grab"
+    newEl.textContent = "ITEM"
+    containerItem.appendChild(newEl)
+  })
+})
+
