e5a8260bf2e4c8f333ce4d3e111452f09aa3a70a
Pengpa0012 <peng.ilikha@gmail.com>
2024-01-30 09:53:33 +0800

    added initial items

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-const items = document.querySelectorAll(".item")
 const form = document.querySelector("#form")
 const sections = document.querySelectorAll(".section")
 const overlay = document.querySelector(".overlay")
 const addTask = document.querySelector(".add-task")
+
+const allItems = [
+  {
+    title: "Item 1",
+    description: "This is a description",
+    column: 1
+  },
+  {
+    title: "Item 2",
+    description: "This is a description",
+    column: 1
+  },
+  {
+    title: "Item 3",
+    description: "This is a description",
+    column: 3
+  }
+]
+
+sections.forEach(el => {
+
+  allItems.forEach(item => {
+    const newItem = document.createElement("div")
+    newItem.className = "item border p-2 rounded-md bg-[#e4e4e4] w-full cursor-grab" 
+    newItem.setAttribute("draggable", true)
+    newItem.textContent = item.title
+    console.log(el.attributes["data-column"].value)
+    if(el.attributes["data-column"].value == item.column) {
+      el.appendChild(newItem)
+    }
+  })
+
+  el.addEventListener('dragover', e => {
+    const draggable = document.querySelector('.dragging')
+    el.appendChild(draggable)    
+  })  
+})
 
 document.addEventListener('click', e => {
   if (!form.contains(e.target) && !addTask.contains(e.target)) {
@@ -25,6 +61,8 @@ addTask.addEventListener("click", () => {
   overlay.classList.remove("hide")
 })
 
+const items = document.querySelectorAll(".item")
+
 items.forEach(el => {
   el.addEventListener("dragstart", e => {
     e.target.classList.add('dragging')
@@ -38,9 +76,3 @@ items.forEach(el => {
   })
 })
 
-sections.forEach(el => {
-  el.addEventListener('dragover', e => {
-    const draggable = document.querySelector('.dragging')
-    el.appendChild(draggable)    
-  })  
-})
